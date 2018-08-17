5d7e01ed3893335d432cbe82a40713b7ac6a1e5e
yanghao <yanghao7@asiainfo.com>
2018-08-17 10:01:04 +0800

    heartBeat代码编写v1

--- messagecenter/src/main/java/com/qianli/ilink/cloud_platform/messagecenter/heartbeat/service/UdpServerNodeManager.java
+++ messagecenter/src/main/java/com/qianli/ilink/cloud_platform/messagecenter/heartbeat/service/UdpServerNodeManager.java
@@ -1,12 +1,13 @@
 package com.qianli.ilink.cloud_platform.messagecenter.heartbeat.service;
 
 import com.qianli.ilink.cloud_platform.messagecenter.heartbeat.bean.UdpServerNode;
+import com.qianli.ilink.cloud_platform.messagecenter.heartbeat.config.HeartBeatConfig;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
@@ -15,6 +16,9 @@ import java.util.stream.Collectors;
 @Slf4j
 @Component
 public class UdpServerNodeManager {
+
+    @Autowired
+    private HeartBeatConfig heartBeatConfig;
 
     private Map<String,UdpServerNode> udpServerNodeMap = new ConcurrentHashMap<>();
     
@@ -43,4 +47,37 @@ public class UdpServerNodeManager {
         return udpServerNodeList.parallelStream().filter(udpServerNode -> udpServerNode.getStatus() == 0).collect(Collectors.toList());
     }
 
+    public void refreshWithSuccess(String udpServer) {
+        String ip = udpServer.split(":")[0];
+        Integer port = Integer.parseInt(udpServer.split(":")[1]);
+        //如果成功，强制刷新node.
+        refresh(udpServer,UdpServerNode.builder().ip(ip).port(port).retryTimes(0).status(0).build());
+    }
+
+    public void refreshWithFail(String udpServer) {
+        Integer retryTimes = heartBeatConfig.getRetryTimes();
+        String ip = udpServer.split(":")[0];
+        Integer port = Integer.parseInt(udpServer.split(":")[1]);
+        //如果失败，先判断是否含有node
+        UdpServerNode node = get(udpServer);
+        //如果包含node,则对失败次数加1 ps:失败3次以内认为有效
+        if(node == null){
+            //因为有node初始化,这段逻辑可能不会被执行
+            refresh(udpServer,UdpServerNode.builder().ip(ip).port(port).retryTimes(1).status(0).build());
+        }else{
+            if(node.getStatus() == 1){
+                log.error("node {}:{} is invalid...",ip,port);
+                return;
+            }
+            int currentRetryTimes = node.getRetryTimes()+1;
+            if(currentRetryTimes > retryTimes){
+                node.setStatus(1);
+                log.error("node {}:{} is invalid...",ip,port);
+            }else{
+                node.setRetryTimes(retryTimes);
+                log.warn("node {}:{} retry {} times...",ip,port,retryTimes);
+            }
+            refresh(udpServer,node);
+        }
+    }
 }
--- messagecenter/src/main/java/com/qianli/ilink/cloud_platform/messagecenter/heartbeat/service/impl/HeartBeatServiceImpl.java
+++ messagecenter/src/main/java/com/qianli/ilink/cloud_platform/messagecenter/heartbeat/service/impl/HeartBeatServiceImpl.java
@@ -1,6 +1,5 @@
 package com.qianli.ilink.cloud_platform.messagecenter.heartbeat.service.impl;
 
-import com.qianli.ilink.cloud_platform.messagecenter.heartbeat.bean.UdpServerNode;
 import com.qianli.ilink.cloud_platform.messagecenter.heartbeat.config.HeartBeatConfig;
 import com.qianli.ilink.cloud_platform.messagecenter.heartbeat.service.HeartBeatSender;
 import com.qianli.ilink.cloud_platform.messagecenter.heartbeat.service.UdpServerNodeManager;
@@ -12,10 +11,7 @@ import org.springframework.util.StringUtils;
 import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Optional;
 
 
 @Slf4j
@@ -35,48 +31,22 @@ public class HeartBeatServiceImpl implements HeartBeatSender {
             log.error("heartBeat udpServerList 配置项异常...");
             return;
         }
-        Integer retryTimes = heartBeatConfig.getRetryTimes();
-        if(retryTimes == null){
-            log.error("heartBeat retryTimes 配置项异常...");
-            return;
-        }
         Arrays.asList(udpServerListStr.split(",")).parallelStream().forEach(udpServer ->{
-            String ip = udpServer.split(":")[0];
-            Integer port = Integer.parseInt(udpServer.split(":")[1]);
-            boolean isSuccess = this.sendHeartBeat(ip,port);
+            boolean isSuccess = this.sendHeartBeat(udpServer);
             if(isSuccess){
-                //如果成功，强制刷新node.
-                udpServerNodeManager.refresh(udpServer,UdpServerNode.builder().ip(ip).port(port).retryTimes(0).status(0).build());
+                udpServerNodeManager.refreshWithSuccess(udpServer);
             }else{
-                //如果失败，先判断是否含有node
-               UdpServerNode node = udpServerNodeManager.get(udpServer);
-               //如果包含node,则对失败次数加1 ps:失败3次以内认为有效
-               if(node == null){
-                   //因为有node初始化,这段逻辑可能不会被执行
-                   udpServerNodeManager.refresh(udpServer,UdpServerNode.builder().ip(ip).port(port).retryTimes(1).status(0).build());
-               }else{
-                   if(node.getStatus() == 1){
-                       log.error("node {}:{} is invalid...",ip,port);
-                       return;
-                   }
-                   int currentRetryTimes = node.getRetryTimes()+1;
-                   if(currentRetryTimes > retryTimes){
-                       node.setStatus(1);
-                       log.error("node {}:{} is invalid...",ip,port);
-                   }else{
-                       node.setRetryTimes(retryTimes);
-                       log.warn("node {}:{} retry {} times...",ip,port,retryTimes);
-                   }
-                   udpServerNodeManager.refresh(udpServer,node);
-               }
+                udpServerNodeManager.refreshWithFail(udpServer);
             }
         });
     }
 
-    private boolean sendHeartBeat(String ip, Integer port) {
+    private boolean sendHeartBeat(String udpServer) {
         boolean flag;
         Socket client = null;
         try {
+            String ip = udpServer.split(":")[0];
+            Integer port = Integer.parseInt(udpServer.split(":")[1]);
             client = new Socket();
             InetSocketAddress address = new InetSocketAddress(ip,port);
             client.connect(address, 1000);
