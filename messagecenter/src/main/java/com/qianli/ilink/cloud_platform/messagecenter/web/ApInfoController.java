004da4c1fb80db81092a5221a38f0deb6a71161a
yanghao <yanghao7@asiainfo.com>
2018-08-18 23:58:54 +0800

    4 ap http数据接收。

--- messagecenter/src/main/java/com/qianli/ilink/cloud_platform/messagecenter/web/ApInfoController.java
+++ messagecenter/src/main/java/com/qianli/ilink/cloud_platform/messagecenter/web/ApInfoController.java
@@ -4,12 +4,14 @@ import com.googlecode.protobuf.format.JsonFormat;
 import com.qianli.ilink.cloud_platform.messagecenter.enums.MessageType;
 import com.qianli.ilink.cloud_platform.messagecenter.pojo.dto.*;
 import com.qianli.ilink.cloud_platform.messagecenter.service.MessageSender;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class ApInfoController {
 
@@ -19,7 +21,8 @@ public class ApInfoController {
     @RequestMapping(value = "/apbaseinfo", method = RequestMethod.POST)
     public void apBaseInfoReceive(@RequestBody ApBaseInfoProto.ApBaseInfo apBaseInfo) {
         if(apBaseInfo == null){
-
+            log.info("api apbaseinfo request protobuf body is null...");
+            return;
         }
         messageSender.send(Message.builder().type(MessageType.AP_BASE_INFO.getValue()).body(new JsonFormat().printToString(apBaseInfo)).build());
     }
@@ -27,7 +30,8 @@ public class ApInfoController {
     @RequestMapping(value = "/apstatusinfo", method = RequestMethod.POST)
     public void apStatusInfoReceive(@RequestBody ApStatusInfoProto.ApStatusInfo apStatusInfo) {
         if(apStatusInfo == null){
-
+            log.info("api apstatusinfo request protobuf body is null...");
+            return;
         }
         messageSender.send(Message.builder().type(MessageType.AP_STATUS_INFO.getValue()).body(new JsonFormat().printToString(apStatusInfo)).build());
     }
@@ -35,7 +39,8 @@ public class ApInfoController {
     @RequestMapping(value = "/aponlinestainfo", method = RequestMethod.POST)
     public void apOnlineStaInfoReceive(@RequestBody ApOnlineStaInfoProto.TotalApOnlineStaInfo totalApOnlineStaInfo) {
         if(totalApOnlineStaInfo == null){
-
+            log.info("api aponlinestainfo request protobuf body is null...");
+            return;
         }
         messageSender.send(Message.builder().type(MessageType.AP_ONLINE_STA_INFO.getValue()).body(new JsonFormat().printToString(totalApOnlineStaInfo)).build());
     }
@@ -43,7 +48,8 @@ public class ApInfoController {
     @RequestMapping(value = "/apofflinestainfo", method = RequestMethod.POST)
     public void apOfflineStaInfoReceive(@RequestBody ApOfflineStaInfoProto.TotalApOfflineStaInfo totalApOfflineStaInfo) {
         if(totalApOfflineStaInfo == null){
-
+            log.info("api apofflinestainfo request protobuf body is null...");
+            return;
         }
         messageSender.send(Message.builder().type(MessageType.AP_OFFLINE_STA_INFO.getValue()).body(new JsonFormat().printToString(totalApOfflineStaInfo)).build());
     }
