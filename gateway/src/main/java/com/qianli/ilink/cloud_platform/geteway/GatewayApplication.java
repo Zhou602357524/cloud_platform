dcc7d7d09aa7508500d07c8b3a64f7c69e2c2c76
yanghao <yanghao7@asiainfo.com>
2018-08-21 18:42:36 +0800

    oauth2.0授权服务器  gateway 权限拦截 v1

--- gateway/src/main/java/com/qianli/ilink/cloud_platform/geteway/GatewayApplication.java
+++ gateway/src/main/java/com/qianli/ilink/cloud_platform/geteway/GatewayApplication.java
@@ -5,8 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
-    public static void main(String[] args) {
 
+    public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
+
 }
